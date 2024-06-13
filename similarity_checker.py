87c382231a4bdbf6330f368dffc60af0dcc426b8
heehoha <dolgyuk@gmail.com>
2024-06-13 16:01:55 +0900

    refactor: illegal argument check >> function

--- similarity_checker.py
+++ similarity_checker.py
@@ -1,6 +1,9 @@
 class SimilarityChecker:
     def get_score(self, first, second):
-        if not isinstance(first, str) or not isinstance(second, str):
+        if self.assert_illegal_argument(first, second):
             raise TypeError()
 
         return 100
+
+    def assert_illegal_argument(self, first, second):
+        return not isinstance(first, str) or not isinstance(second, str)
