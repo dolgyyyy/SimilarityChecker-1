6260445f5793079dfea7b93855dfc7023dabe660
heehoha <dolgyuk@gmail.com>
2024-06-13 16:00:56 +0900

    feat: illegal argument check

--- similarity_checker.py
+++ similarity_checker.py
@@ -1,3 +1,6 @@
 class SimilarityChecker:
     def get_score(self, first, second):
+        if not isinstance(first, str) or not isinstance(second, str):
+            raise TypeError()
+
         return 100
--- test_similarity_checker.py
+++ test_similarity_checker.py
@@ -8,5 +8,20 @@ class TestSimilarityChecker(TestCase):
         super().setUp()
         self.similarity_checker = SimilarityChecker()
 
+    def assert_illegal_argument(self, first, second):
+        try:
+            self.similarity_checker.get_score(first, second)
+            self.fail()
+        except TypeError:
+            pass
+
+    def test_illegal_argument(self):
+        self.assert_illegal_argument(None, "ABCDE")
+        self.assert_illegal_argument("ABCDE", None)
+        self.assert_illegal_argument("ABCDE", 12345)
+        self.assert_illegal_argument(12345, "ABCDE")
+        self.assert_illegal_argument("ABCDE", [])
+        self.assert_illegal_argument([], "ABCDE")
+
     def test_get_score(self):
         self.assertEqual(self.similarity_checker.get_score("ABCDE", "ABCDE"), 100)
