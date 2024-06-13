09a4dfbbf8afb5cf42c68409989303fb3924f7a4
heehoha <dolgyuk@gmail.com>
2024-06-13 15:46:44 +0900

    init: templete file

---
+++ similarity_checker.py
@@ -0,0 +1,2 @@
+class Similarity:
+    pass
---
+++ test_similarity_checker.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestSimilarity(TestCase):
+    def test_similarity_checker(self):
+        self.assertEqual(1, 1)
